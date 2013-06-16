0f2631cde934ac3dd6ea6bc16440ee39db6ca803
Shashwat Kandadai <shashwatak@gmail.com>
2013-06-15 21:26:21 -0700

    Fixed the other TweenJS opupdate callback

--- js/services.js
+++ js/services.js
@@ -351,7 +351,7 @@ function ThreeJS(WorkerManager) {
     var target_position = ecf_array_to_webgl_pos(position_ecf);
     var tween = new TWEEN.Tween(start_position).to(target_position, 500);
     tween.onUpdate(function(){
-      if (sat_table[satnum]) {
+      if (sat_table[satnum] && sat_table[satnum]["marker_ecf"]) {
         sat_table[satnum]["marker_ecf"].position = start_position;
       };
     });
