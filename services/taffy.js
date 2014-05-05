65828e001dc742f2b04ed4f1f28bd01b3887f2f7
Putnameshere <wye2@ucsc.edu>
2014-05-04 21:40:12 -0700

    Added in new files for db

---
+++ services/taffy.js
@@ -0,0 +1,21 @@
+function taffy(Worker Manager){
+var json,db;
+
+//load db from localstorage 	
+
+function load_db(){
+json = require('data.json');
+db = TAFFY(json); 
+};
+
+//find info in sat
+function find_info(satnum){
+
+
+};
+
+
+
+
+
+};
