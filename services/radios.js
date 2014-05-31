f4f2c0f598f377eb7d9869f91bbf7c5eebd49ab6
Putnameshere <wye2@ucsc.edu>
2014-05-31 14:15:24 -0700

    Added comments in radio.js to edit later in lab

--- services/radios.js
+++ services/radios.js
@@ -62,8 +62,6 @@ function Radios(WorkerManager) {
 		};
 	};
 
-	//Where the data is being read. Fix this(?)
-	//Probably. 
 	//The motors module might need something like this(????)
 	function radio_comms_async_loop(sat_item) {
 		//console.log("*~*~* What does this radio comms async loop do??? *~*~*");
@@ -74,7 +72,7 @@ function Radios(WorkerManager) {
 			//Since this is not polling anymore, we will change the way we detect background tuning.   
 			if(sat_item["is_tracking"]) { //auto tuning
 				autoFlag = true;
-				sat_item["radio_main_frequency"] = Math.floor(sat_item["doppler_factor"] * sat_item["radio_main_frequency"]);
+				sat_item["radio_main_frequency"] = Math.floor(sat_item["doppler_factor"] * sat_item["radio_main_frequency"]); //might need to change this 
 				sat_item["radio_sub_frequency"] = Math.floor(sat_item["doppler_factor"] * sat_item["radio_sub_frequency"]); 
 				
                 origFreq = sat_item["radio_sub_frequency"]; //to fix the bug in resetting frequency after auto-tuning
@@ -137,94 +135,7 @@ function Radios(WorkerManager) {
 	};
 
 	
-	
-	
-	//New function to connect radio
-	/*
-	
-	function connect_radio(COMport, radio_type){
-	
-	function on_open(open_info) {
-			console.log("doing it");
-			if(open_info.connectionId > 0) {
-				sat_table[satnum] = {
-					connectionId: open_info.connectionId,
-					COMport: COMport,
-					functions: supported_radios[radio_type]["functions"],
-					callback: callback
-				};
 
-				radioConnectionId = sat_table[satnum]["connectionId"];
-				console.log("RADIO CONNECTED: radioConnectionId=" + radioConnectionId);
-
-				console.log("this is your uplink " + uplink);
-				console.log("this is your downlink " + downlink);
-				//This function definitely works. 
-				sat_table[satnum]["uplink_frequency"] = uplink;
-				sat_table[satnum]["downlink_frequency"] = downlink;
-				sat_table[satnum]["background_tuning_wait_start"] = 0;
-				sat_table[satnum]["offsetFlag"] = false;
-				sat_table[satnum]["offset"] = 0;
-
-				// something here with the callback function? DUDE WHAT IS GOING ON.
-				chrome.serial.flush(sat_table[satnum]["connectionId"], function (result) {
-					if(result) {
-						//console.log("Hello? Flush callback function?");
-						save_satnum(satnum);
-						console.log("MAIN FREQ PART");
-
-						sat_table[satnum]["functions"].get_main_frequency( sat_table[satnum]["connectionId"],
-							function (radio_main_frequency) {
-								sat_table[satnum]["functions"].get_sub_frequency(sat_table[satnum]["connectionId"], function (radio_sub_frequency) {
-									if(sat_table[satnum]["radio_main_frequency"] !== radio_main_frequency ||
-										sat_table[satnum]["radio_sub_frequency"] !== radio_sub_frequency) {
-										// That means that since the last time we set the frequency,
-										// somebody else (the user) changed the dials.
-										sat_table[satnum]["background_tuning_wait_start"] = performance.now();
-										sat_table[satnum]["radio_main_offset"] += (radio_main_frequency - sat_table[satnum]["uplink_frequency"]);
-										sat_table[satnum]["radio_sub_offset"] += (radio_sub_frequency - sat_table[satnum]["downlink_frequency"]);
-									}
-
-									console.log("This is the radio_main_freq response: " + radio_main_frequency);
-
-
-								});
-
-							}//end callback function. All of this is irrelevant to the motors part. Moving on....
-							);
-
-						console.log("HERE IS YOUR DATA: " + '\n' + sat_table[satnum]["radio_main_frequency"] + '\n' + sat_table[satnum]["radio_sub_frequency"]);
-						setTimeout(function () {
-							radio_comms_async_loop(sat_table[satnum]);
-						}, 3000);
-
-					} else {
-						console.log("Couldn't Flush: " + COMport);
-					};
-				});
-			} else {
-				console.log("Couldn't Open: " + COMport);
-			};
-		};
-	
-	
-	//This connects the USB to radio on COMport 
-		console.log("radios.connect_radios(" + COMport + ", " + radio_type + ", " + satnum + ")");
-		if(COMport && supported_radios[radio_type] && supported_radios[radio_type]["bitrate"]) {
-			chrome.serial.connect(COMport, {
-				bitrate: supported_radios[radio_type]["bitrate"]
-			}, on_open);
-			radio_comms_async_loop(sat_table[satnum]);
-
-		} else {
-			console.log("tough luck kid");
-		};
-	
-	};
-*/
-	
-	
-	//old function to connect to radio
 	
 	function connect_radio(satnum, COMport, radio_type, callback, uplink, downlink) {
 		save_satnum(satnum);
@@ -418,10 +329,10 @@ function Radios(WorkerManager) {
 						sat_table[keeper]["radio_sub_frequency"] = parseInt(parse_results);
 						console.log(origSubFreq +" " +  origFreq + " " +  parseInt(parse_results) +"THIS IS YOUR THINGY ");
 						if(origFreq - parseInt(parse_results) > 0) { //shifting down
-							sat_table[keeper]["radio_main_frequency"] = origSubFreq + Math.abs((origFreq - parseInt(parse_results)));
+							sat_table[keeper]["radio_main_frequency"] = origSubFreq + Math.abs((origFreq - parseInt(parse_results))); //need to change this
 
 						} else { //shifting up
-							sat_table[keeper]["radio_main_frequency"] = origSubFreq - Math.abs((origFreq - parseInt(parse_results)));
+							sat_table[keeper]["radio_main_frequency"] = origSubFreq - Math.abs((origFreq - parseInt(parse_results))); //need to change this
 
 
 						}
